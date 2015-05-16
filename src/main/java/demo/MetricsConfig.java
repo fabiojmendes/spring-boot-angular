96bc1ec7b35962b4593df718da0d67357ed54f48
Fabio Mendes <fabiojmendes@gmail.com>
2015-05-16 13:24:35 -0400

    Datadog Metrics

--- src/main/java/demo/MetricsConfig.java
+++ src/main/java/demo/MetricsConfig.java
@@ -1,42 +1,24 @@
 package demo;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import javax.servlet.Filter;
 import javax.servlet.ServletContextListener;
 
+import org.coursera.metrics.datadog.DatadogReporter;
+import org.coursera.metrics.datadog.transport.HttpTransport;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-import com.codahale.metrics.ConsoleReporter;
-import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
-import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.codahale.metrics.servlet.InstrumentedFilter;
 import com.codahale.metrics.servlet.InstrumentedFilterContextListener;
 
 @Configuration
 public class MetricsConfig {
-
-	//@Bean
-	public ConsoleReporter consoleReporter(MetricRegistry registry) {
-		ConsoleReporter reporter = ConsoleReporter.forRegistry(registry)
-				.convertRatesTo(TimeUnit.SECONDS)
-				.convertDurationsTo(TimeUnit.MILLISECONDS)
-				.build();
-		reporter.start(5, TimeUnit.SECONDS);
-		return reporter;
-	}
-
-	@Bean
-	public JmxReporter jmxReporter(MetricRegistry registry) {
-		registry.registerAll(new GarbageCollectorMetricSet());
-		registry.registerAll(new MemoryUsageGaugeSet());
-		JmxReporter reporter = JmxReporter.forRegistry(registry).build();
-		reporter.start();
-		return reporter;
-	}
 
 	@Bean
 	public ServletContextListener webMetricListener(final MetricRegistry registry) {
@@ -51,5 +33,32 @@ public class MetricsConfig {
 	@Bean
 	public Filter webMetricFilter() {
 		return new InstrumentedFilter();
+	}
+
+	@Configuration
+	@Profile("production")
+	private static class DatadogMetricConfig {
+
+		@Value("${HOSTNAME}")
+		private String hostname;
+
+		@Value("${APP_NAME}")
+		private String appName;
+
+		@Value("${DATADOG_API_KEY}")
+		private String datadogApiKey;
+
+		@Bean
+		public DatadogReporter datadogReporter(MetricRegistry registry) {
+			HttpTransport httpTransport = new HttpTransport.Builder().withApiKey(datadogApiKey).build();
+			DatadogReporter reporter = DatadogReporter.forRegistry(registry)
+					.withHost(hostname)
+					.withTags(Arrays.asList(appName))
+					.withTransport(httpTransport)
+					.build();
+			reporter.start(5, TimeUnit.SECONDS);
+			return reporter;
+		}
+
 	}
 }
