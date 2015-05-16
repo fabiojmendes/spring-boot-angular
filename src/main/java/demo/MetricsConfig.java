b67409d71438a1cba730d7503d3743e7f01ddae0
Fabio Mendes <fabiojmendes@gmail.com>
2015-05-16 17:18:36 -0400

    MetricsConfig

--- src/main/java/demo/MetricsConfig.java
+++ src/main/java/demo/MetricsConfig.java
@@ -8,17 +8,31 @@ import javax.servlet.ServletContextListener;
 
 import org.coursera.metrics.datadog.DatadogReporter;
 import org.coursera.metrics.datadog.transport.HttpTransport;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.JvmAttributeGaugeSet;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.codahale.metrics.servlet.InstrumentedFilter;
 import com.codahale.metrics.servlet.InstrumentedFilterContextListener;
 
 @Configuration
 public class MetricsConfig {
+
+	@Bean
+	public InitializingBean metricsSetup(final MetricRegistry registry) {
+		return () -> {
+			registry.registerAll(new GarbageCollectorMetricSet());
+			registry.registerAll(new MemoryUsageGaugeSet());
+			registry.registerAll(new JvmAttributeGaugeSet());
+		};
+	}
 
 	@Bean
 	public ServletContextListener webMetricListener(final MetricRegistry registry) {
@@ -33,6 +47,17 @@ public class MetricsConfig {
 	@Bean
 	public Filter webMetricFilter() {
 		return new InstrumentedFilter();
+	}
+
+	@Configuration
+	@Profile("!production")
+	static class JmxMetricConfig {
+		@Bean
+		public JmxReporter jmxReporter(MetricRegistry registry) {
+			JmxReporter reporter = JmxReporter.forRegistry(registry).build();
+			reporter.start();
+			return reporter;
+		}
 	}
 
 	@Configuration
