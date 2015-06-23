ea942d422ee36ceb3b02520717c026fad94a6100
Fábio Mendes <fabiojmendes@gmail.com>
2015-06-22 20:22:35 -0400

    Notify the application startup to systemd

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,13 +1,26 @@
 package demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		notifyStartup();
 	}
 
+	private static void notifyStartup() {
+		try {
+			Process notify = new ProcessBuilder("systemd-notify --ready").start();
+			notify.waitFor();
+		} catch (Exception e) {
+			logger.warn("Error notifying the application Startup: [{}] {}", e.getClass(), e.getMessage());
+		}
+	}
 }
